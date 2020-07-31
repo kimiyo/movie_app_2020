6d7db0233b2359bcf9f8ddc44b0a628738b90503
JHKim <kimiyo@naver.com>
2020-07-31 21:16:25 +0900

    Updates

---
+++ precache-manifest.6d1b1f79f5e46c14edfa96452e5a09b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "529e0ce38ce22eccc4076058c4ef1de0",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "0a705a1d645661dde0d0",
+    "url": "/movie_app_2020/static/css/main.677aedcc.chunk.css"
+  },
+  {
+    "revision": "af44dae07ad93b1049cb",
+    "url": "/movie_app_2020/static/js/2.ada4d0d5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2020/static/js/2.ada4d0d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a705a1d645661dde0d0",
+    "url": "/movie_app_2020/static/js/main.d1104cb0.chunk.js"
+  },
+  {
+    "revision": "3fd90deccac5a1cac62a",
+    "url": "/movie_app_2020/static/js/runtime-main.4d01fe54.js"
+  }
+]);
